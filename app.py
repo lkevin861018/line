a2b756f5aca840405d392af31bff67c6b66c4bea
wen <kevinliang1018@gmail.com>
2024-04-11 14:03:30 +0800

    ''

--- app.py
+++ app.py
@@ -31,7 +31,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    if event.message.text == '銀河':
+        message = TextSendMessage(text='大便')    
+    else:
+        message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
