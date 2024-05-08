07f6f4d036d25e4cea224bf780a84e81d2b9f12d
wen <kevinliang1018@gmail.com>
2024-05-08 21:11:17 +0800

    'dollar'

--- dollar.py
+++ dollar.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def dollar(cointype):
-    url = f'https://www.google.com/search?q={cointype}'
+    url = f'https://www.google.com/search?q={cointype}換台幣'
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
     resp = requests.get(url, headers={
         'user-agent': user_agent
