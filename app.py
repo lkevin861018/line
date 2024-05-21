d41126ccc53ac96b8640fd0e28a5fbeae1c56bc7
wen <kevinliang1018@gmail.com>
2024-05-21 10:31:51 +0800

    'upload'

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-import os
+import os,tempfile
 import random as rd
 from dotenv import load_dotenv
 import dollar
@@ -35,55 +35,80 @@ def callback():
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=(TextMessage,ImageMessage))
 def handle_message(event):
-    url = 'https://linegg.onrender.com'
-    if event.message.text == '銀河':
-        message = TextSendMessage(text='大便')   
-    elif event.message.text == '妲黑':
-        message = ImageSendMessage(
-            original_content_url = url+'/static/dahate.png',
-            preview_image_url = url+'/static/dahate.png'
+    if isinstance(event.message,TextMessage):
+        url = 'https://linegg.onrender.com'
+        if event.message.text == '銀河':
+            message = TextSendMessage(text='大便')   
+        elif event.message.text == '妲黑':
+            message = ImageSendMessage(
+                original_content_url = url+'/static/dahate.png',
+                preview_image_url = url+'/static/dahate.png'
+                )
+        elif event.message.text == '凸':
+            random_num = rd.randint(1,2)
+            message = ImageSendMessage(
+                original_content_url = url+'/static/fk/fk%d.jpg'%random_num,
+                preview_image_url = url+'/static/fk/fk%d.jpg'%random_num
+                )
+        elif event.message.text == '抽':
+            random_num = rd.randint(1,63)
+            message = ImageSendMessage(
+                original_content_url = url+'/static/roll/%d.png'%random_num,
+                preview_image_url = url+'/static/roll/%d.png'%random_num
+                )
+        elif event.message.text.find('消夜') >= 0 or event.message.text.find('宵夜') >= 0:
+            message = TextSendMessage('麥當勞辣味雞塊')
+        elif event.message.text == '開台啦':
+            message = TextSendMessage('各位妲寶，妲妲開台啦 https://www.twitch.tv/dada_0124 !💕💕')
+        
+    ###########################################################################
+
+        
+        if event.message.text.find('!匯率') == 0 or event.message.text.find('！匯率') == 0:
+            message = TextSendMessage(
+                dollar.dollar(event.message.text.split(' ')[1])
             )
-    elif event.message.text == '凸':
-        random_num = rd.randint(1,2)
-        message = ImageSendMessage(
-            original_content_url = url+'/static/fk/fk%d.jpg'%random_num,
-            preview_image_url = url+'/static/fk/fk%d.jpg'%random_num
-            )
-    elif event.message.text == '抽':
-        random_num = rd.randint(1,63)
-        message = ImageSendMessage(
-            original_content_url = url+'/static/roll/%d.png'%random_num,
-            preview_image_url = url+'/static/roll/%d.png'%random_num
-            )
-    elif event.message.text.find('消夜') >= 0 or event.message.text.find('宵夜') >= 0:
-        message = TextSendMessage('麥當勞辣味雞塊')
-    elif event.message.text == '開台啦':
-        message = TextSendMessage('各位妲寶，妲妲開台啦 https://www.twitch.tv/dada_0124 !💕💕')
-    
-###########################################################################
 
-    
-    if event.message.text.find('!匯率') == 0 or event.message.text.find('！匯率') == 0:
-        message = TextSendMessage(
-            dollar.dollar(event.message.text.split(' ')[1])
-        )
+        if event.message.text.find('!股票') == 0 or event.message.text.find('！股票') == 0:
+            message = TextSendMessage(
+                dollar.stock(event.message.text.split(' ')[1])
+            )    
 
-    if event.message.text.find('!股票') == 0 or event.message.text.find('！股票') == 0:
-        message = TextSendMessage(
-            dollar.stock(event.message.text.split(' ')[1])
-        )    
+    ###########################################################################
 
-###########################################################################
+        if event.message.text == '!指令':
+            message = TextSendMessage(text='銀河\n妲黑\n抽\n凸\n!匯率\n!股票\n消夜 宵夜\n')   
 
-    if event.message.text == '!指令':
-        message = TextSendMessage(text='銀河\n妲黑\n抽\n凸\n!匯率\n!股票\n消夜 宵夜\n')   
+        if message:
+            line_bot_api.reply_message(event.reply_token, message)
 
-    if message:
-        line_bot_api.reply_message(event.reply_token, message)
+    ###########################################################################
 
-import os
+    if isinstance(event.message, ImageMessage):
+        static_upload_path = os.path.join(os.path.dirname(__file__), 'static', 'upload')
+        ext = 'jpg'
+        message_content = line_bot_api.get_message_content(event.message.id)
+        with tempfile.NamedTemporaryFile(dir=static_upload_path, prefix=ext + '-', delete=False) as tf:
+            for chunk in message_content.iter_content():
+                tf.write(chunk)
+            tempfile_path = tf.name
+
+        dist_path = tempfile_path + '.' + ext
+        dist_name = os.path.basename(dist_path)
+        os.rename(tempfile_path, dist_path)
+        try:
+            path = os.path.join('static', 'upload', dist_name)
+            print(path)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='上傳成功'))
+        except:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='上傳失敗'))
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug = False)
     
