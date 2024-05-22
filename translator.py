2da3794b64be746aa34d2ce843c3617b0bb9a20d
wen <kevinliang1018@gmail.com>
2024-05-22 15:19:16 +0800

    'langbug'

--- translator.py
+++ translator.py
@@ -1,12 +1,12 @@
 from googletrans import Translator
 import json
 
-trans = Translator()
+transcode = Translator()
 with open('language.json','r',encoding = 'utf8') as f:
     lang_dict = json.load(fp = f)
 def trans(txt,lang):
     try:
-        trans_txt = trans.translate(txt,dest = lang_dict[lang]).text
+        trans_txt = transcode.translate(txt,dest = lang_dict[lang]).text
         return trans_txt
     except:
         return '換種語言試試'
