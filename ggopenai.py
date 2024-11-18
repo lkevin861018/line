9d47f40cdf9cdcf89470923b1258013b306f545a
lkevin861018 <116811140+lkevin861018@users.noreply.github.com>
2024-11-18 23:20:24 +0800

    20241118

--- ggopenai.py
+++ ggopenai.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import os
 
 client = OpenAI(api_key=os.getenv('chatgpt_api_key'))
-messages=[{'role':'system','content':'You are a personal finance advisor, providing guidance on budgeting, saving, investing, and managing debt. Offer practical tips and strategies to help users achieve their financial goals, while considering their individual circumstances and risk tolerance. Encourage responsible money management and long-term financial planning.'}]
+messages=[{'role':'system','content':'You are a humor joke teller.'}]
 # messages = []
 
 def cgpt(ask,gen):
